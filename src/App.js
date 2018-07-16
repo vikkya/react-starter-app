d108fdf6b4c6faf9dd8d586cd498cdbf803c8c40
Vikky Ambarkar <ytubeindia2@gmail.com>
2018-07-16 21:49:01 +0530

    Add babelrc file and changed index.js file location

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Start editing from here</h2>
+                <p>goto src/App.js file to start working</p>
+            </div>
+        )
+    }
+}
+
+export default App;
